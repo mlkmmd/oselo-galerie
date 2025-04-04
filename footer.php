68fc993efaa6784ca362cca2efa5232834e92103
Malik <malikmahammed2003@gmail.com>
2025-04-04 11:26:56 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div>
+    </main>
+    
+    <footer>
+        <div class="container">
+            <p>&copy; <?= date('Y') ?> <?= APP_NAME ?> - Developed by Malik MAHAMMED</p>
+        </div>
+    </footer>
+</body>
+</html>
+
